ed7dd3cc9b16829af7c1a1f83f819a49d8173142
Clement Gouilloud <clement.gouilloud@gmail.com>
2025-10-27 23:29:54 +0100

    Ajout gradient linéaire

--- App.js
+++ App.js
@@ -1,18 +1,45 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { StyleSheet} from 'react-native';
 
 import RootNavigator from './navigation/RootNavigator';
 import { UserProvider } from './contexts/UserContext';
 import { RecipeProvider } from './contexts/RecipeContext';
+import { InventoryProvider } from './contexts/InventoryContext';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
     <UserProvider>
       <RecipeProvider>
-        <NavigationContainer>
-          <RootNavigator />
-        </NavigationContainer>
+        <InventoryProvider>
+          <LinearGradient
+                  colors={['rgb(252, 228, 252)', 'rgb(218, 228, 255)']}
+                  start={{ x: 0, y: 0 }}
+                  end={{ x: 1, y: 1 }}
+                  style={styles.container}
+                >
+            <NavigationContainer  
+                theme={MyTheme}>
+              <RootNavigator />
+            </NavigationContainer>
+          </LinearGradient>
+        </InventoryProvider>
       </RecipeProvider>
     </UserProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: 'transparent',
+  },
+};
